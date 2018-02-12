861b2924e1bc99d8938a68c79bf1a763cc734e70
Zach Posten <zposten@gmail.com>
2018-02-12 17:45:11 -0600

    Configure webpack to bundle our code
    There is no source yet, but webpack is now configured to intelligently
    bundle our source together once it's written.  The code may be split
    into multiple bundles to reduce page load time.  Images may also
    automatically be inlined in html to avoid an http request if they're
    small enough.
    
    Unlike some other bundlers, due to its ability to add additional
    loaders, webpack can also bundle non JavaScript files like CSS, SASS, or
    markdown!

--- bld/srcServer.js
+++ bld/srcServer.js
@@ -1,18 +1,20 @@
 import express from 'express'
 import path from 'path'
 import open from 'open'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
 
-var port = 3000;
-var app = express();
+const port = 3000
+const app = express()
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
-});
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+})
 
 app.listen(port, function(err) {
   if (err) {
-    console.log(err);
+    console.log(err)
   } else {
-    open('http://localhost:' + port);
+    open('http://localhost:' + port)
   }
-});
+})
---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+export default {
+  debug: true,
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/js/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
