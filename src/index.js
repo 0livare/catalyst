6224d751fde66966a5b6bdc8668e301b909ad3c2
Zach Posten <zposten@gmail.com>
2018-02-16 23:01:41 -0600

    Make calls to a mock API to fill HTML table
    The API is currently just an endpoint on our development server,
    returning static JSON.
    
    With this change, I also eliminated the js/ and css/ folders that I had
    created.  Similarily to the reasoning to put the tests right next to the
    source files, I think that it's benefitial to have the CSS files right
    next to the JS files.  This allows for simple import paths,
    non-duplication of directory structures, and makes it obvious that the
    files all go together.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+import {getUsers} from './api/userApi'
+
+// Populate table of users via API call
+getUsers().then(result => {
+  let usersBody = ''
+
+  result.forEach(user => {
+    usersBody += `
+      <tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>
+    `
+  })
+
+  global.document.getElementById('users').innerHTML = usersBody
+})
+
