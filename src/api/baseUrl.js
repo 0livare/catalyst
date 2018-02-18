21b60aabc6a4e610a289627b546ffffa5485212d
Zach Posten <zposten@gmail.com>
2018-02-18 00:49:38 -0600

    Use specific query parameter to enable mock api
    Instead of just checking for 'localhost', we now check for an explicity
    query string parameter.  This allows us to easily switch between the
    mock api (json-server) and the "real" api (express in srcServer.js).
    
    Although there clearly is no "real" api at the moment, the express
    server in srcServer.js is taking its place for the moment.

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,20 @@
 export default function getBaseUrl() {
-  const inDev = window.location.hostname === 'localhost'
-  return inDev ? 'http://localhost:3001/' : '/'
+  return getQueryStringParameterByName('useMockApi')
+    ? 'http://localhost:3001/'
+    : '/'
+}
+
+// This should really be done with a tested and maintained framework
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href
+
+  name = name.replace(/[\[\]]/g, "\\$&")
+
+  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+  let results = regex.exec(url)
+
+  if (!results)    return null
+  if (!results[2]) return ''
+
+  return decodeURIComponent(results[2].replace(/\+/g, " "))
 }
