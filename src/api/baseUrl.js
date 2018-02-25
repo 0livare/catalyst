8777fe89fd8e4d268d2502afb9ad15728e6a0907
Zach Posten <zposten@gmail.com>
2018-02-25 00:25:33 -0600

    Force constant usage of the mock API
    I think it's benefitial to have this baseUrl class around to show how
    one can logically switch between a real API and our mock one when that
    is necessary.  For the purpose of this starter kit however, doing so
    will never be necessary, so I'm commenting out that functionality.

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,20 +1,21 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') || true
-    ? 'http://localhost:3001/'
-    : '/'
+  return 'http://localhost:3001/'
+  // return getQueryStringParameterByName('useMockApi')
+  //   ? 'http://localhost:3001/'
+  //   : '/'
 }
 
 // This should really be done with a tested and maintained framework
-function getQueryStringParameterByName(name, url) {
-  if (!url) url = window.location.href
+// function getQueryStringParameterByName(name, url) {
+//   if (!url) url = window.location.href
 
-  name = name.replace(/[\[\]]/g, "\\$&")
+//   name = name.replace(/[\[\]]/g, "\\$&")
 
-  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
-  let results = regex.exec(url)
+//   let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+//   let results = regex.exec(url)
 
-  if (!results)    return null
-  if (!results[2]) return ''
+//   if (!results)    return null
+//   if (!results[2]) return ''
 
-  return decodeURIComponent(results[2].replace(/\+/g, " "))
-}
+//   return decodeURIComponent(results[2].replace(/\+/g, " "))
+// }
