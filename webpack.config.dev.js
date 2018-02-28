cc669d10c21044ca4d7e178021df5cb850d70ec6
Zach Posten <zposten@gmail.com>
2018-02-27 23:23:58 -0600

    Add support for typescript

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,53 @@ import webpack from 'webpack'
 
 
 export default {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-hot-middleware/client',
+    path.resolve(__dirname, 'src/index.tsx'),
+  ],
   devtool: 'eval-source-map',
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+
+    // Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true, // Automatically inject any necessary script tags
+    }),
+  ],
+  module: {
+    rules: [
+      {test: /\.tsx?$/, exclude: /node_modules/, use: ['babel-loader', 'awesome-typescript-loader']},
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, use: [
+        'style-loader',
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+        'postcss-loader',
+      ]},
+      {test: /\.scss$/, use: [ // Note that these loaders are applied in reverse order, the last one first
+        'style-loader', // 5. Add CSS to DOM by injecting style tag
+        // 4. Use CSS modules, generating class names in the specified format
+        // 3. Load the CSS file, resolve imports and urls
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+        'postcss-loader', // 2. Perform any transformations specified in postcss.config.js
+        'sass-loader',    // 1. Compile SASS into CSS
+      ]},
+    ]
+  },
   stats: {
     assets: false,
     cached: false,
@@ -31,42 +77,4 @@ export default {
     version: false,
     warnings: false
   },
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-hot-middleware/client',
-    path.resolve(__dirname, 'src/index'),
-  ],
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'src'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-
-    // Create HTML file that includes reference to bundled JS
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: true, // Automatically inject any necessary script tags
-    }),
-  ],
-  module: {
-    rules: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      {test: /\.css$/, use: [
-        'style-loader',
-        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-        'postcss-loader',
-      ]},
-      {test: /\.scss$/, use: [ // Note that these loaders are applied in reverse order, the last one first
-        'style-loader', // 5. Add CSS to DOM by injecting style tag
-        // 4. Use CSS modules, generating class names in the specified format
-        // 3. Load the CSS file, resolve imports and urls
-        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-        'postcss-loader', // 2. Perform any transformations specified in postcss.config.js
-        'sass-loader',    // 1. Compile SASS into CSS
-      ]},
-    ]
-  }
 }
