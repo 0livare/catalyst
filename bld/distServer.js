b931054d7865fb0f6f5973d57b1da971bcea9ea6
Zach Posten <Zach.Posten@jci.com>
2018-03-15 15:52:17 -0500

    Update production course information

--- bld/distServer.js
+++ bld/distServer.js
@@ -39,26 +39,23 @@ app.listen(port, function(err) {
 let courses = [
   {
     "id": "b002ec4f-209d-5fb7-953c-fdf42c8b24bf",
-    "title": "cillum labore anim enim id",
-    "watchUrl": "http://alayna.name",
-    "authorId": "Taya24",
-    "length": 520,
-    "category": "Redux"
+    "title": "A psychological study of Dr. Seuss",
+    "authorId": "cat-in-the-hat",
+    "length": 115215,
+    "category": "Psychology",
   },
   {
     "id": "fbc31ac4-738d-55fd-800e-04e9a21506d7",
-    "title": "zach is cool",
-    "watchUrl": "http://efren.net",
-    "authorId": "Magdalena56",
-    "length": 334,
-    "category": "Redux"
+    "title": "An introduction to Astrophysics",
+    "authorId": "carl-sagen",
+    "length": 10000000001,
+    "category": "Science",
   },
   {
     "id": "6433da25-82f5-5890-b5c2-c9d5b86f1fe0",
-    "title": "adipisicing tempor consequat aute dolor",
-    "watchUrl": "https://lucienne.com",
-    "authorId": "Nola87",
-    "length": 939,
-    "category": "React"
+    "title": "On the art of awesomeness",
+    "authorId": "zach-posten",
+    "length": 4444,
+    "category": "Life",
   },
 ]
